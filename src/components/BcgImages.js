f370e143a32a2e226d8e748b8f26a47926b71352
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-12-22 08:44:56 +0100

    bcg is done

--- src/components/BcgImages.js
+++ src/components/BcgImages.js
@@ -1,7 +1,51 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import lightMobileBcg from '../images/bg-mobile-light.jpg';
+import lightDesktopBcg from '../images/bg-desktop-light.jpg';
+import darkMobileBcg from '../images/bg-mobile-dark.jpg';
+import darkDesktopBcg from '../images/bg-desktop-dark.jpg';
+import styled from 'styled-components';
 
 const BcgImages = () => {
-  return <div>bcg images</div>;
+  const { theme } = useSelector((state) => state.reducer);
+
+  if (theme) {
+    return (
+      <>
+        <Wrapper
+          src={lightMobileBcg}
+          alt="light-mobile-bcg"
+          className="mobile"
+        />
+        <Wrapper
+          src={lightDesktopBcg}
+          alt="light-desktop-bcg"
+          className="desktop"
+        />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Wrapper src={darkMobileBcg} alt="dark-mobile-bcg" className="mobile" />
+        <Wrapper
+          src={darkDesktopBcg}
+          alt="dark-desktop-bcg"
+          className="desktop"
+        />
+      </>
+    );
+  }
 };
+
+const Wrapper = styled.img`
+  &.mobile {
+    display: block;
+  }
+
+  &.desktop {
+    display: none;
+  }
+`;
 
 export default BcgImages;
